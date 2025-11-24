387720b345aae829da73e32aed9cbe3fccd54e0b
wildcherry2 <tylercwray@gmail.com>
2025-11-23 22:01:51 -0500

    implement asset loader

--- app/src/main/java/com/example/chatauth/fragment/chat/WebviewOwnerFragment.java
+++ app/src/main/java/com/example/chatauth/fragment/chat/WebviewOwnerFragment.java
@@ -1,17 +1,21 @@
 package com.example.chatauth.fragment.chat;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.webkit.WebViewAssetLoader;
 
 public class WebviewOwnerFragment extends Fragment {
-    public WebView webview;
     public static final String TAG = "WebviewOwnerFragment";
 
     @Override
@@ -19,8 +23,20 @@ public class WebviewOwnerFragment extends Fragment {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
         if (webview == null) {
-            webview = new WebView(requireContext().getApplicationContext()); // use application context to prevent leaking the activity on config changes
+            Context ctx = requireContext().getApplicationContext();
+            asset_loader = new WebViewAssetLoader.Builder()
+                    .addPathHandler("/assets/", new WebViewAssetLoader.AssetsPathHandler(ctx))
+                    .build();
+            webview = new WebView(ctx); // use application context to prevent leaking the activity on config changes
             webview.getSettings().setJavaScriptEnabled(true);
+            webview.setWebViewClient(new WebViewClient() {
+                @Nullable
+                @Override
+                public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                    return asset_loader.shouldInterceptRequest(request.getUrl());
+                }
+            });
+            //todo add args as interface
             webview.loadUrl("https://appassets.androidplatform.net/assets/index.html");
         }
     }
@@ -30,4 +46,7 @@ public class WebviewOwnerFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return null;
     }
+
+    private WebView webview;
+    private WebViewAssetLoader asset_loader;
 }
