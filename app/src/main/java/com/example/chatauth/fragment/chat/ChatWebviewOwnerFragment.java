b69d5da2259aba1e48f2fca1ce66d3413aac2c0b
wildcherry2 <tylercwray@gmail.com>
2025-11-24 00:19:17 -0500

    add fragment that displays webview persisted by ChatWebviewOwnerFragment update nav graph

--- app/src/main/java/com/example/chatauth/fragment/chat/ChatWebviewOwnerFragment.java
+++ app/src/main/java/com/example/chatauth/fragment/chat/ChatWebviewOwnerFragment.java
@@ -18,15 +18,10 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.webkit.WebViewAssetLoader;
 
-public class WebviewOwnerFragment extends Fragment {
+public class ChatWebviewOwnerFragment extends Fragment {
     public static final String TAG = "WebviewOwnerFragment";
 
-    public WebView getWebview() { return webview; }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setRetainInstance(true);
+    public WebView load(String userId, String userName) {
         if (webview == null) {
             Context ctx = requireContext().getApplicationContext();
             asset_loader = new WebViewAssetLoader.Builder()
@@ -41,58 +36,27 @@ public class WebviewOwnerFragment extends Fragment {
                     return asset_loader.shouldInterceptRequest(request.getUrl());
                 }
             });
-            var args_bundle = getArguments();
-            if(args_bundle == null) throw new RuntimeException("Can't construct webview owner without args!");
-            args_bundle.setClassLoader(Arguments.class.getClassLoader());
-            var args = args_bundle.getParcelable("args", Arguments.class);
-            if(args == null) throw new RuntimeException("Can't construct webview owner without args!");
-            webview.addJavascriptInterface(new AndroidBridge(args.userId, args.userName), "AndroidBridge");
-            webview.loadUrl("https://appassets.androidplatform.net/assets/index.html");
+//            var args_bundle = getArguments();
+//            if(args_bundle == null) throw new RuntimeException("Can't construct webview owner without args!");
+//            args_bundle.setClassLoader(Arguments.class.getClassLoader());
+//            var args = args_bundle.getParcelable("args", Arguments.class);
+//            if(args == null) throw new RuntimeException("Can't construct webview owner without args!");
         }
+        webview.addJavascriptInterface(new AndroidBridge(userId, userName), "AndroidBridge");
+        webview.loadUrl("https://appassets.androidplatform.net/assets/index.html");
+        return webview;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return null;
-    }
-
-    public static class Arguments implements Parcelable {
-        public final String userId;
-        public final String userName;
-
-        public Arguments(String userId, String userName) {
-            this.userId = userId;
-            this.userName = userName;
-        }
-
-        protected Arguments(Parcel in) {
-            userId = in.readString();
-            userName = in.readString();
-        }
-
-        @Override
-        public int describeContents() {
-            return 0;
-        }
-
-        @Override
-        public void writeToParcel(@NonNull Parcel parcel, int i) {
-            parcel.writeString(userId);
-            parcel.writeString(userName);
-        }
-
-        public static final Creator<Arguments> CREATOR = new Creator<Arguments>() {
-            @Override
-            public Arguments createFromParcel(Parcel in) {
-                return new Arguments(in);
-            }
-
-            @Override
-            public Arguments[] newArray(int size) {
-                return new Arguments[size];
-            }
-        };
     }
 
     private WebView webview;
