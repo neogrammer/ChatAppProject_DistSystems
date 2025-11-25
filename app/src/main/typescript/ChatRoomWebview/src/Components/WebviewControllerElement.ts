bbbc2b754e6c749e0512b629465954dfad14f352
wildcherry2 <tylercwray@gmail.com>
2025-11-24 20:27:48 -0500

    add DLOG and implement message sending

--- app/src/main/typescript/ChatRoomWebview/src/Components/WebviewControllerElement.ts
+++ app/src/main/typescript/ChatRoomWebview/src/Components/WebviewControllerElement.ts
@@ -5,22 +5,23 @@ import { IWebviewController } from "../Interfaces/IWebviewController";
 import { IChatMessage } from "../Interfaces/IChatMessage";
 import { IChatRoom } from "../Interfaces/IChatRoom";
 import { ChatRoomElement } from "./ChatRoomElement";
-import { customElement, state } from "lit/decorators.js";
+import { customElement, query, state } from "lit/decorators.js";
 import { ChatMessage } from "../Generated/chat";
+import { randomUUID } from "crypto";
 
 @customElement("webview-controller")
 export class WebviewControllerElement extends LitElement implements IWebviewController {
     addRoom(room: IChatRoom): boolean {
         if(!room) {
-            //todo log 
+            DLOG("[WebviewControllerElement] Failed to add room because argument was nullish!");
             return false;
         }
         if(!room.id || room.id.length === 0) {
-            //todo log
+            DLOG("[WebviewControllerElement] Failed to add room because argument id was null or 0 length!");
             return false;
         }
         if(!room.roomName || room.roomName.length === 0) {
-            //todo log
+            DLOG("[WebviewControllerElement] Failed to add room because argument roomName was null or 0 length!");
             return false;
         }
         if(this._rooms.has(room.id)) return false;
@@ -31,13 +32,13 @@ export class WebviewControllerElement extends LitElement implements IWebviewCont
     }
 
     removeRoom(roomId: string): boolean {
-        if(!roomId) {
-            //todo log
+        if(!roomId || roomId.length === 0) {
+            DLOG("[WebviewControllerElement] Failed to remove room because argument was null or 0 length!");
             return false;
         }
         if(roomId === this.current_room) {
             if(this._rooms.size === 1) {
-                //todo log
+                DLOG("[WebviewControllerElement] Failed to remove room because doing so would leave the app roomless!");
                 return false;
             }
             const as_array = [...this._rooms];
@@ -51,35 +52,35 @@ export class WebviewControllerElement extends LitElement implements IWebviewCont
     }
 
     switchToRoom(roomId: string): boolean {
-        if(!roomId) {
-            //todo log
+        if(!roomId || roomId.length === 0) {
+            DLOG("[WebviewControllerElement] Failed to switch to room because argument was null or 0 length!");
             return false;
         }
         if(this._rooms.has(roomId)) {
             this.current_room = roomId;
             return true;
         }
-        //todo log
+        DLOG(`[WebviewControllerElement] Failed to switch to room because roomId '${roomId}' doesn't exist!`);
         return false;
     }
 
     addMessage(message: ChatMessage): boolean {
         if(!message) {
-            //todo log
+            DLOG("[WebviewControllerElement] Failed to add message because argument was nullish!");
             return false;
         }
         const room_id = message.roomId;
         const room = this._rooms.get(room_id);
         if(!room) {
-            //todo log
+            DLOG(`[WebviewControllerElement] Failed to add message because room id couldn't be found! Message: ${message}`);
             return false;
         }
         return room.addMessage(message);
     }
 
     addMessages(...messages: ChatMessage[]): boolean {
-        if(!messages) {
-            //todo log
+        if(!messages || messages.length === 0) {
+            DLOG("[WebviewControllerElement] Failed to add messages because argument was nullish or array was empty!");
             return false;
         }
         let all_true = true;
@@ -91,21 +92,25 @@ export class WebviewControllerElement extends LitElement implements IWebviewCont
     }
 
     removeMessage(messageId: string, roomId: string): boolean {
-        if(!messageId || !roomId) {
-            //todo log
+        if(!messageId || messageId.length === 0) {
+            DLOG(`[WebviewControllerElement] Failed to remove message because messageId argument was nullish or empty! roomId: ${roomId}`)
+            return false;
+        }
+        if(!roomId || roomId.length === 0) {
+            DLOG(`[WebviewControllerElement] Failed to remove message because roomId argument was nullish or empty! messageId: ${messageId}`)
             return false;
         }
         const room = this._rooms.get(roomId);
         if(!room) {
-            //todo log
+            DLOG(`[WebviewControllerElement] Failed to remove message because roomId '${roomId}' doesn't exist! messageId: ${messageId}`)
             return false;
         }
         return room.removeMessage(messageId);
     }
 
     removeMessages(...messageIds: [messageId: string, roomId: string][]): boolean {
-        if(!messageIds) {
-            //todo log
+        if(!messageIds || messageIds.length === 0) {
+            DLOG("[WebviewControllerElement] Failed to remove messages because argument array was null or empty!");
             return false;
         }
         let all_true = true;
@@ -118,13 +123,13 @@ export class WebviewControllerElement extends LitElement implements IWebviewCont
 
     hasMessage(messageId: string, roomId?: string): boolean {
         if(!messageId) {
-            //todo log
+            DLOG(`[WebviewControllerElement] Failed to check for message because messageId argument was nullish or empty! roomId: ${roomId}`)
             return false;
         }
         if(roomId && roomId.length !== 0) {
             const room = this._rooms.get(roomId);
             if(!room) {
-                //todo log
+                DLOG(`[WebviewControllerElement] Failed to remove message because room doesn't exist for roomId '${roomId}'! messageId: ${messageId}`)
                 return false;
             }
             return room.hasMessage(messageId);
@@ -169,6 +174,32 @@ export class WebviewControllerElement extends LitElement implements IWebviewCont
     @state()
     private current_room = "";
 
+    @query("#msg_input", true)
+    private _input!: HTMLInputElement;
+
+    private _onSend() {
+        if(!this._input.value || this._input.value.length === 0) return;
+        const message: ChatMessage = {
+            content: this._input.value,
+            createdAt: Date.now(),
+            roomId: this.current_room,
+            userId: window.AndroidBridge.getUserId(),
+            userName: window.AndroidBridge.getUserName(),
+            id: crypto.randomUUID()
+        }
+        
+        const room = this._rooms.get(this.current_room);
+
+        if(!room) {
+            DLOG(`[WebviewControllerElement] Failed to send message because roomId '${this.current_room}' no longer exists!`);
+        }
+
+        if(!room!.addMessage(message)) return;
+
+        const encoded = ChatMessage.encode(message).finish();
+        window.AndroidBridge.postMessage(btoa(String.fromCharCode(...encoded)));
+    }
+
     protected override render() {
         if(this.current_room.length === 0) return html``;
         const room = this._rooms.get(this.current_room);
@@ -181,7 +212,7 @@ export class WebviewControllerElement extends LitElement implements IWebviewCont
             ${room}
             <div id="footer">
                 <input id="msg_input" placeholder="Send message..."/>
-                <svg id="send_btn" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#789DE5"><path d="M0 0h24v24H0z" fill="none"/><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"/></svg>
+                <svg @click="${this._onSend}" id="send_btn" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#789DE5"><path d="M0 0h24v24H0z" fill="none"/><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"/></svg>
             </div>
         `
     }
