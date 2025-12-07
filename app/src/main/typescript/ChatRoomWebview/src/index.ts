a07a05883d4c841743aebe6b4846737f5453de09
wildcherry2 <tylercwray@gmail.com>
2025-12-07 18:36:08 -0500

    feat: Add room switching UI
    Adds a side menu that allows users to view the list of chat rooms and switch between them.
    
    Changes include:
    - A hamburger menu button in the header to open a popover.
    - The popover displays a list of available chat rooms.
    - Clicking a room name in the list switches the view to that room.
    - General style updates to support the new UI components.
    - An API for `requestMessageHistory` has been added to the Android bridge mock.

--- app/src/main/typescript/ChatRoomWebview/src/index.ts
+++ app/src/main/typescript/ChatRoomWebview/src/index.ts
@@ -47,30 +47,34 @@ if(DEBUG) {
     }
   }
 
-  // window.AndroidBridge = {
-  //   getUserId() {
-  //     return "0"
-  //   },
-  //   getUserName() {
-  //     return "Tyler"
-  //   },
-  //   postMessage(b64) {
+  window.AndroidBridge ??= {
+    getUserId() {
+      return "0"
+    },
+    getUserName() {
+      return "Tyler"
+    },
+    postMessage(b64) {
       
-  //   },
-  //   setLoaded() {
+    },
+    setLoaded() {
       
-  //   },
-  // }
+    },
+    requestMessageHistory(ChatMessageHistoryRequest_b64) {
+      
+    },
+  }
 
   window.DLOG = (val) => console.debug(val);
 
-  // const controller = document.querySelector('webview-controller');
-  // controller!.addRoom({id: "0", roomName: "Test Room"});
-  // //controller!.switchToRoom("0");
-  // controller!.addMessage(makeDummyMessage("0", "0", "0", "Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message ", "Tyler"));
-  // setTimeout(() => {
-  //   controller!.addMessage(makeDummyMessage("1", "0", "2", "Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message ", "User"));
-  // }, 1000);
+  const controller = document.querySelector('webview-controller');
+  controller!.addRoom({id: "main", roomName: "Anonymous"});
+  controller!.addRoom({id: "second", roomName: "Second Room"});
+  //controller!.switchToRoom("0");
+  controller!.addMessage(makeDummyMessage("0", "main", "0", "Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message ", "Tyler"));
+  setTimeout(() => {
+    controller!.addMessage(makeDummyMessage("1", "main", "2", "Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message Test message ", "User"));
+  }, 1000);
   
 }
 
